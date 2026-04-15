b8f1103b28bc924a109222bdbcf66c2ecd7464cc
Daniel <dwcronin@umass.edu>
2026-04-15 19:47:59 -0400

    feat: add rsvp controller file and imports

---
+++ src/rsvp/RsvpController.ts
@@ -0,0 +1,6 @@
+// src/rsvp/RsvpController.ts
+
+import type { Response } from "express";
+import type { IAppBrowserSession, AppSessionStore } from "../session/AppSession.js";
+import type { RsvpService } from "./RsvpService.js";
+import type { RsvpError } from "./Rsvp.js";
