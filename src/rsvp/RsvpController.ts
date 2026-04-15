d9a0b86c0d7ac92a22c534150b65839eaa9eaea9
Daniel <dwcronin@umass.edu>
2026-04-15 19:56:34 -0400

    feat: add controller interface and class skeleton

--- src/rsvp/RsvpController.ts
+++ src/rsvp/RsvpController.ts
@@ -4,3 +4,16 @@ import type { Response } from "express";
 import type { IAppBrowserSession, AppSessionStore } from "../session/AppSession.js";
 import type { RsvpService } from "./RsvpService.js";
 import type { RsvpError } from "./Rsvp.js";
+
+export interface IRsvpController {
+  toggleRsvp(
+    res: Response,
+    eventId: string,
+    session: IAppBrowserSession,
+    store: AppSessionStore,
+  ): Promise<void>;
+}
+
+class RsvpController implements IRsvpController {
+  constructor(private readonly service: RsvpService) {}
+  }
