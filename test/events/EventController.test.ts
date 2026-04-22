f49dafef0492cd317fc54a5a49a2ce2f07178ce7
Daniel <dwcronin@umass.edu>
2026-04-21 20:27:17 -0400

    feat: add jest imports

---
+++ test/events/EventController.test.ts
@@ -0,0 +1,6 @@
+import request from "supertest";
+import type { Express } from "express";
+import { createComposedApp } from "../../src/composition";
+import * as EventRepo from "../../src/events/InMemoryEventRepository";
+import * as RsvpRepo from "../../src/rsvp/InMemoryRsvpRepository";
+import type { Event } from "../../src/events/Event";
